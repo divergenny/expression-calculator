3bf0d96f346f24f4a13d04ed9d744b3f2b471610
Divergenny <divergenny@gmail.com>
2019-10-17 20:46:49 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,38 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let positionStart = 0;
+    let positionEnd = 0;
+
+	for (let i = 0; i < expr.length; i++) {
+		if (expr[i] == '(') {
+			positionStart += 1;
+        }
+        
+		if (expr[i] == ')') {
+			positionEnd += 1;
+		}
+    }
+    
+	if  (positionStart != positionEnd) {
+		throw new Error('ExpressionError: Brackets must be paired');
+    }
+    
+    let output = undefined;
+    let x = expr.replace(/\s/g, '');
+    let sortValue = String(x).match(/(^[0-9*\/\\(\\)+-]+$)/);
+    let res = new Function('return ' + sortValue);
+	
+    for (let i = 0; i <= expr.length; i++) {
+    	if ( expr[i]  == '/' ) { 
+	    	if ( expr[i+2] == '0' ) {
+	    		throw new Error('TypeError: Division by zero.');
+	    	}
+		} else {
+			output = res();
+		}
+    } 
+    return  output;
 }
 
 module.exports = {
